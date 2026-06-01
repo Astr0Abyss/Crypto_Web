926bb47f0be50adf9ca912d927bf5561d5f88e70
Shahriar <shahriarxox124@proton.me>
2026-06-01 14:17:35 +0600

    restore algorithms and soft background

--- js/algorithms/index.js
+++ js/algorithms/index.js
@@ -1,16 +1,15 @@
 import { caesarCipher } from "./caesar.js";
 import { shiftCipher } from "./shift.js";
 import { rsaCipher } from "./rsa.js";
-// Hidden for now. Keep the files available so they can be added back later.
-// import { hillCipher } from "./hill.js";
-// import { vigenereCipher } from "./vigenere.js";
+import { hillCipher } from "./hill.js";
+import { vigenereCipher } from "./vigenere.js";
 
 export const algorithms = {
   caesar: caesarCipher,
   shift: shiftCipher,
   rsa: rsaCipher,
-  // vigenere: vigenereCipher,
-  // hill: hillCipher,
+  vigenere: vigenereCipher,
+  hill: hillCipher,
 };
 
 if (typeof window !== "undefined") {
